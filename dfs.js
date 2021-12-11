8143a52ae43bd983399b62cd01b22b45912e2f2f
Prateek Sharma <prateeksharma@Prateeks-MacBook-Air.local>
2021-12-12 03:59:51 +0530

    initial commit

---
+++ dfs.js
@@ -0,0 +1,44 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const depthFirstValues = (root) => {
+    // todo
+    let ans = []
+    let stack = [root]
+
+    while (stack.length) {
+        let currentNode = stack.pop()
+        ans.push(currentNode.val)
+        if (currentNode.right) stack.push(currentNode.right)
+        if (currentNode.left) stack.push(currentNode.left)
+    }
+    return ans
+};
+
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+console.log(depthFirstValues(a))
+//    -> ['a', 'b', 'd', 'e', 'c', 'f']
